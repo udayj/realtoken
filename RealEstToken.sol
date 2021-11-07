891fb69c8fe457457bb5d4c6a2f24f9ab7569327
udayj <jhunjhunwala.uday@gmail.com>
2021-11-07 16:16:47 +0530

    removed minting restriction for ease of demoing

--- RealEstToken.sol
+++ RealEstToken.sol
@@ -7,14 +7,14 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract RealEstToken is ERC20, Ownable {
     uint256 private _cap;
     constructor(uint256 _cap1) ERC20("RealEstToken","REAL") {
-        _mint(msg.sender, 100000 * 10 ** decimals());
-        _cap=_cap1;
+        _mint(msg.sender, 1000 * 10 ** decimals());
+        _cap=_cap1*10**decimals();
     }
 
     function cap() public view returns(uint256) {
         return _cap;
     }
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) public {
         require(ERC20.totalSupply() + amount <= cap(), "ERC20Capped: cap exceeded");
         _mint(to, amount);
     }
